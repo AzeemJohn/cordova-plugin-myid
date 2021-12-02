6cd180f6bb09c4092d71eb3e8c426e802d5adf1b
AzeemJohn <34554229+AzeemJohn@users.noreply.github.com>
2021-12-02 12:01:35 +0500

    Update myid.js

--- www/myid.js
+++ www/myid.js
@@ -10,3 +10,19 @@ CordovaPluginMyid.yourFunctionName = function(onSuccess, onError) {
    exec(onSuccess, onError, PLUGIN_NAME, "yourFunctionName", []);
 };
 module.exports = CordovaPluginMyid;
+
+
+'use strict';
+
+Object.defineProperty(exports, "_esModule", {
+ value: true
+});
+
+export.isSupported = isSupported;
+export.startReception = startReception;
+export.stopReception = stopReception;
+export.requestPermission = requestPermission;
+
+var _exec = require('cordova/exec');
+
+var _exec2
