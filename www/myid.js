20519465c472e5c62a2271f73f53bb473c191fc6
AzeemJohn <34554229+AzeemJohn@users.noreply.github.com>
2021-12-03 15:34:47 +0500

    Update myid.js

--- www/myid.js
+++ www/myid.js
@@ -1,16 +1,17 @@
 var exec = require('cordova/exec');
 
-// var PLUGIN_NAME = "CordovaPluginMyid"; // This is just for code completion uses.
+ var PLUGIN_NAME = "CordovaPluginMyid"; // This is just for code completion uses.
 
-// var CordovaPluginMyid = function() {}; // This just makes it easier for
-//  us to export all of the functions at once.
-// // All of your plugin functions go below this. 
-// // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-// CordovaPluginMyid.yourFunctionName = function(onSuccess, onError) {
-//    exec(onSuccess, onError, PLUGIN_NAME, "yourFunctionName", []);
-// };
-// module.exports = CordovaPluginMyid;
+ var CordovaPluginMyid = function() {}; // This just makes it easier for
+  us to export all of the functions at once.
+ // All of your plugin functions go below this.
+ // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
 
-exports.createWallet = function(agr0, success, error) {
- exec(success, error, "CordovaPluginMyid", "test", [agr0];
+//exports.createWallet = function(agr0, success, error) {
+// exec(success, error, "CordovaPluginMyid", "test", [agr0];
+
+ CordovaPluginMyid.yourFunctionName = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "test", [agr0]);
+ };
+ module.exports = CordovaPluginMyid;
 };
