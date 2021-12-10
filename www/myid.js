4a32e640e8159da0ef56bf19e35bf8c345857d52
AzeemJohn <34554229+AzeemJohn@users.noreply.github.com>
2021-12-10 17:28:37 +0500

    Update myid.js

--- www/myid.js
+++ www/myid.js
@@ -2,8 +2,7 @@ var exec = require('cordova/exec');
 
  var PLUGIN_NAME = "CordovaPluginMyid"; // This is just for code completion uses.
 
- var CordovaPluginMyid = function() {}; // This just makes it easier for
-  us to export all of the functions at once.
+ var CordovaPluginMyid = function() {}; // This just makes it easier for us to export all of the functions at once.
  // All of your plugin functions go below this.
  // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
 
@@ -12,6 +11,5 @@ var exec = require('cordova/exec');
 
  CordovaPluginMyid.test = function(onSuccess, onError) {
     exec(onSuccess, onError, PLUGIN_NAME, "test", [agr0]);
- };
 };
 module.exports = CordovaPluginMyid;
