f4bffde351b7582d5748ce64c8bc1b40a19b574b
AzeemJohn <34554229+AzeemJohn@users.noreply.github.com>
2021-12-24 15:48:28 +0500

    Create ViewController.swift

---
+++ src/iOS/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var authTypeSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        var urlComps = URLComponents(string: "https://myid.uz/api/v1/oauth2/authorization")!
+        let queryItems = [
+            URLQueryItem(name: "client_id", value: YOU_CLINET_ID),
+            URLQueryItem(name: "redirect_uri", value: "uzinfocom://bank"),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "method", value: authTypeSwitch.isOn ? "strong" : "simple"),
+            URLQueryItem(name: "scope", value: "common_data,doc_data,contacts,address"),
+        ]
+        
+        urlComps.queryItems = queryItems
+        
+        UIApplication.shared.open(urlComps.url!)
+    }
+}
